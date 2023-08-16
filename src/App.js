6cd38dbd49b40d181a083e49e9576901ec0a1c74
Ananya Gupta <ananyag2806@gmail.com>
2023-08-16 19:15:46 +0530

    scroll snap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
 import { useState, useEffect } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
+import { useRef } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import useScrollSnap from './useScrollSnap';
+import './style.css';
 
 function App() {
 	const [result, setResult] = useState('');
+	const containerRef = React.createRef();
 
-	useEffect(() => {
-		const openai = new OpenAIApi(
-			new Configuration({
-				apiKey: process.env.REACT_APP_API_KEY,
-			})
-		);
+	const [bind, unbind] = useScrollSnap(
+		containerRef,
+		{ snapDestinationY: '90%' },
+		() => console.log('snapped')
+	);
+	// useEffect(() => {
+	// 	const openai = new OpenAIApi(
+	// 		new Configuration({
+	// 			apiKey: process.env.REACT_APP_API_KEY,
+	// 		})
+	// 	);
 
-		openai
-			.createChatCompletion({
-				model: 'gpt-3.5-turbo',
-				messages: [
-					{
-						role: 'user',
-						content: 'Hello ChatGpt',
-					},
-				],
-			})
-			.then((res) => {
-				console.log(res.data.choices[0].message.content);
-				setResult(res.data.choices[0].message.content);
-			});
-	}, []);
-	return <div className='App'>{result}</div>;
+	// 	openai
+	// 		.createChatCompletion({
+	// 			model: 'gpt-3.5-turbo',
+	// 			messages: [
+	// 				{
+	// 					role: 'user',
+	// 					content: 'Hello ChatGpt',
+	// 				},
+	// 			],
+	// 		})
+	// 		.then((res) => {
+	// 			console.log(res.data.choices[0].message.content);
+	// 			setResult(res.data.choices[0].message.content);
+	// 		});
+	// }, []);
+
+	return (
+		<div id='container' ref={containerRef}>
+			<div className='page first-page'>
+				<div>I</div>
+				<div className='hint'>scroll down</div>
+			</div>
+			<div className='page second-page'>
+				<div>II</div>
+			</div>
+			<div className='page third-page'>
+				<div>III</div>
+			</div>
+			<div className='page fourth-page'>
+				<div>IV</div>
+				<div className='hint'>scroll up</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
