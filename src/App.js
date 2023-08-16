aa740f018b2dc3107effedcb099ba5b0d68834de
Ananya Gupta <ananyag2806@gmail.com>
2023-08-16 15:02:22 +0530

    Done!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
-
-require('dotenv').config();
 
 function App() {
 	const [result, setResult] = useState('');
-	const openai = new OpenAIApi(
-		new Configuration({
-			apiKey: process.env.API_KEY,
-		})
-	);
 
-	openai
-		.createChatCompletion({
-			model: 'gpt-3.5-turbo',
-			messages: [
-				{
-					role: 'user',
-					content: 'Hello ChatGpt',
-				},
-			],
-		})
-		.then((res) => {
-			console.log(res.data.choices[0].message.content);
-			setResult(res.data.choices[0].message.content);
-		});
+	useEffect(() => {
+		const openai = new OpenAIApi(
+			new Configuration({
+				apiKey: process.env.REACT_APP_API_KEY,
+			})
+		);
 
+		openai
+			.createChatCompletion({
+				model: 'gpt-3.5-turbo',
+				messages: [
+					{
+						role: 'user',
+						content: 'Hello ChatGpt',
+					},
+				],
+			})
+			.then((res) => {
+				console.log(res.data.choices[0].message.content);
+				setResult(res.data.choices[0].message.content);
+			});
+	}, []);
 	return <div className='App'>{result}</div>;
 }
 
