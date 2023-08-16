bbf790e8c2c89de080a42347c967b0b9db750d45
Ananya Gupta <ananyag2806@gmail.com>
2023-08-16 23:46:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import useScrollSnap from './useScrollSnap';
 import './style.css';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import { makeStyles } from '@material-ui/core/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: 'center',
+	color: theme.palette.text.secondary,
+}));
+
+const useStyles = makeStyles({
+	root: {
+		display: 'grid',
+	},
+	left: {
+		width: '40%',
+		overflow: 'scroll',
+	},
+	right: {
+		width: '60%',
+	},
+	scroll: {
+		display: 'block',
+	},
+});
 
 function App() {
+	const classes = useStyles();
+
 	const [result, setResult] = useState('');
 	const containerRef = React.createRef();
 
@@ -45,7 +76,16 @@ function App() {
 				<div className='hint'>scroll down</div>
 			</div>
 			<div className='page second-page'>
-				<div>II</div>
+				<div className={classes.left}>
+					<div className={classes.scroll}>
+						akjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdl
+					</div>
+				</div>
+				<div className={classes.right}>
+					<div className={classes.scroll}>
+						oaishlaksdhakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlakjsdf;lkjsd;flkjasd;lkfjdlfkjhoisdfkjahdf
+					</div>
+				</div>
 			</div>
 			<div className='page third-page'>
 				<div>III</div>
