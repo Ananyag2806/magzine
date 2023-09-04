85341cb72d20253794e1668ca1355c08aca4ebce
Ananya Gupta <ananyag2806@gmail.com>
2023-09-04 19:09:00 +0530

    almost done

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,7 @@
 import { useState, useEffect, Fragment } from 'react';
-import { useRef } from 'react';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import useScrollSnap from './useScrollSnap';
 import './style.css';
-import Custom from './Custom';
-import View from './View';
-import Landing from './Landing';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
 import { makeStyles } from '@material-ui/core/styles';
-import image from './img/1.jpg';
-import image2 from './img/2.jpg';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const useStyles = makeStyles({
 	root: {
@@ -24,6 +11,14 @@ const useStyles = makeStyles({
 		position: 'fixed' /* Set the navbar to fixed position */,
 		bottom: '0' /* Position the navbar at the top of the page */,
 		width: '100%' /* Full width */,
+		background: '#f1f1f1',
+	},
+	text: {
+		textAlign: 'center',
+		textDecoration: 'none',
+		fontWeight: '500',
+		margin: '5px',
+		padding: '0',
 	},
 });
 
@@ -32,7 +27,7 @@ function Footer() {
 
 	return (
 		<Fragment>
-			<div className='footer'>
+			<div className={classes.footer}>
 				<h4 className={classes.text}>
 					Made with ⚛ by{' '}
 					<a href='ananyag2806.github.io/portfolio/'>Ananya</a>
